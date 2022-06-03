e25788b8608e1b729956124d9012e60bb85aac50
CZarko <caleb@zarko.org>
2022-06-02 21:50:41 -0700

    jesus updated graphics

--- Assets/Code/Scripts/CreditManager.cs
+++ Assets/Code/Scripts/CreditManager.cs
@@ -3,19 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class CreditManager : MonoBehaviour {
 
     [TextArea(3,10)]
     public string[] dialogue;
 
-    public Text creditText;
+    public AudioClip[] clips;
+    private AudioSource audioSource;
+
+    private TMP_Text textMesh; // textmesh of credit text
+    private Mesh mesh; // mesh of credit text
+    private Vector3[] vertices; // vertices of credit text
     private Queue<string> credits;
 
     // Start is called before the first frame update
     void Start() {
         credits = new Queue<string>(); // instantiate the credit Queue
+        audioSource = this.GetComponent<AudioSource>(); // fetch audio source
+        TMP_Text[] tmps = GetComponentsInChildren<TMP_Text>();
+        foreach(TMP_Text tmp in tmps) textMesh = tmp;
         StartCoroutine(DelayedBegin()); // start the coroutine that will start the credits after a delay
+    }
+
+    // Update is called once per frame
+    void Update() {
+        textMesh.ForceMeshUpdate(); // update text mesh
+        mesh = textMesh.mesh; // get current mesh
+        vertices = mesh.vertices; // get current vertices
+        for(int i = 0; i < textMesh.textInfo.characterCount; ++i) { // for every character in the text
+            TMP_CharacterInfo c = textMesh.textInfo.characterInfo[i]; // get char info
+            
+            int index = c.vertexIndex; // get vertex index on the mesh
+
+            Vector3 offset = Wobble(Time.time + i); // calculate wobbles
+            vertices[index] += offset; // apply offset to all four vertices of char 
+            vertices[index+1] += offset;
+            vertices[index+2] += offset;
+            vertices[index+3] += offset;
+        }
+        mesh.vertices = vertices; // update mesh vertices
+        textMesh.canvasRenderer.SetMesh(mesh); // update renderer
+    }
+
+    Vector2 Wobble(float time) {
+        return new Vector2(Mathf.Sin(time*3.3f), Mathf.Cos(time*2.5f)); // basic wobbling
     }
 
     IEnumerator DelayedBegin() {
@@ -37,14 +70,23 @@ public class CreditManager : MonoBehaviour {
     }
 
     IEnumerator TypeWriter(string s) {
-        creditText.text = ""; // clear the credit text from previous output
-        foreach(char c in s.ToCharArray()) { creditText.text += c; yield return new WaitForSeconds(0.05f); } // add every char one at a time with some delay to creditText
+        textMesh.SetText(""); // clear the credit text from previous output
+        audioSource.clip = clips[Random.Range(0, clips.Length)]; // pick random sound effect
+        audioSource.Play(); // play sound effect
+        foreach(char c in s.ToCharArray()) { 
+            // if either of the following two characters show up richtext change color
+            // and skip to next loop with no delay
+            if(c=='%') {textMesh.SetText(textMesh.text + "<color=#00B7EB>"); continue;}
+            if(c=='$') {textMesh.SetText(textMesh.text + "<color=#424242>"); continue;}
+            textMesh.SetText(textMesh.text+c); // update text with char
+            yield return new WaitForSeconds(0.05f); 
+        } // add every char one at a time with some delay to creditText
         yield return new WaitForSeconds(2f); // wait a couple seconds
         Next(); // begin next
     }
 
     private void End() {
-        creditText.text = ""; // ensures there is no residual text
+        textMesh.SetText(""); // ensures there is no residual text
         SceneManager.LoadScene("Menu"); // exit to main menu
     }
 }
